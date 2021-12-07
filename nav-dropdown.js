2ef4c3eb9188f608481d8b67af2f09c0029dac04
H40N <55667333+H40N@users.noreply.github.com>
2021-12-07 06:14:11 +0100

    arkiv animation
    jag fixa din djävla animation men koden är fördjävlig, fixa sen

--- nav-dropdown.js
+++ nav-dropdown.js
@@ -1,15 +1,16 @@
 
 function dropdownFunction() {
-  document.getElementById("dropdown-arkiv").classList.toggle("show");
+  document.getElementById("dropdown-arkiv").classList.toggle("show-flex");
+  setTimeout(() => { document.getElementById("dropdown-animation").classList.toggle("move"); }, 50);
 }
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("nav-dropdown-content");
+    var dropdowns = document.getElementsByClassName("dropdown-arkiv");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains('show-flex')) {
+        openDropdown.classList.remove('show-flex');
       }
     }
   }
