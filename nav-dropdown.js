dc503b27ff39f87091a0b56ccc143ff2ef55202d
H40N <55667333+H40N@users.noreply.github.com>
2021-12-16 17:14:30 +0100

    mera gerejer
    kdsklkkkjlkfdlk LOL!!!!! javaskript är så grymt mhmm

--- nav-dropdown.js
+++ nav-dropdown.js
@@ -1,7 +1,52 @@
 
 function dropdownFunction() {
   document.getElementById("dropdown-arkiv").classList.toggle("show-flex");
-  setTimeout(() => { document.getElementById("dropdown-animation").classList.toggle("move"); }, 50);
+
+  setTimeout(() => {
+    document.getElementById("dropdown-animation").classList.toggle("move");
+  }, 50);
+
+  var navBar = document.getElementById("navBar");
+  var dropdownArkivEdge = document.getElementById("dropdown-arkiv").getBoundingClientRect();
+
+  navBar.style.height = dropdownArkivEdge.bottom + "px";
+  navBar.style.backgroundImage = "linear-gradient(#1D2951, #1D29519F)"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*if (navBar.style.height == "auto") {
+    document.getElementById("navBar").style.height = dropdownArkivEdge.bottom + "px";
+  } else {
+    document.getElementById("navBar").style.height = "auto";
+  }
+  var i;
+  for (var i = 0; i < navBar.length; i++) {
+    var decisions = navBar[i];
+    if (!decisions.classList.contains('expand')) {
+      document.getElementById("navBar").style.height = dropdownArkivEdge.bottom + "px";
+      document.getElementById("navBar").classList.add("expand");
+    } else if (decisions.classList.contains('expand')) {
+      document.getElementById("navBar").style.height = "auto";
+      document.getElementById("navBar").classList.remove("expand");
+    }
+  }
+
+  console.log(dropdownArkivEdge.top, dropdownArkivEdge.right, dropdownArkivEdge.bottom, dropdownArkivEdge.left);
+  */
 }
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -25,5 +70,5 @@ window.onclick = function(event) {
 }
 
 function moreFuncion() {
-  
+
 }
